2449a856404ab8471280355440e6947b4cc34dde
RachhV <vraq14@gmail.com>
2026-07-07 16:13:14 -0300

    alterei o segundo desafio

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,31 +1,32 @@
 let saldoVitorias = calcularSaldo(35, 0)
-let nivel = undefined
+let nivel = definirNivel(saldoVitorias)
 
 function calcularSaldo(vitorias, derrotas){
-    let saldoVitorias = vitorias - derrotas
-    return saldoVitorias
+    return vitorias - derrotas
 }
 
-if (saldoVitorias <= 10){
-    nivel = "Ferro"
-}
-else if (saldoVitorias <= 20){
-    nivel = "Bronze"
-}
-else if (saldoVitorias <= 50){
-    nivel = "Prata"
-}
-else if (saldoVitorias <= 80){
-    nivel = "Ouro"
-}
-else if (saldoVitorias <= 90){
-    nivel = "Diamante"
-}
-else if (saldoVitorias <= 100){
-    nivel = "Lendário"
-}
-else {
-    nivel = "Imortal"
+function definirNivel(saldoVitorias){
+    if (saldoVitorias <= 10){
+        return "Ferro"
+    }
+    else if (saldoVitorias <= 20){
+        return "Bronze"
+    }
+    else if (saldoVitorias <= 50){
+        return "Prata"
+    }
+    else if (saldoVitorias <= 80){
+        return "Ouro"  
+    }
+    else if (saldoVitorias <= 90){
+        return "Diamante"
+    }
+    else if (saldoVitorias <= 100){
+        return "Lendário"
+    }
+    else {
+        return "Imortal"
+    }
 }
 
 console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de " + nivel)
