a3d7dedee4c5ec2a506fd7d64b438c6cc76b5ad7
Rach V <117462095+RachhV@users.noreply.github.com>
2026-07-16 18:41:20 -0300

    Add files via upload

---
+++ src/03-classes-de-um-heroi.js
@@ -0,0 +1,27 @@
+class Hero {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        if (this.tipo === "mago") {
+            ataque = "magia";
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada";
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais";
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}.`);
+    }
+}
+
+let heroiUm = new Hero("Gandalf", 80, "mago");
+
+heroiUm.atacar();
